0e3e39ba65674f61582b085fcdbcbe9a40d227f5
Madhumitha <madhumithavenu03@gmail.com>
2023-12-27 11:47:46 +0530

    Adding buttons

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,18 @@ import { useSelector } from 'react-redux';
 
 function App() {
   const counter = useSelector((state) => state.counter);
+  function increment(){
+    
+  }
+  function decrement(){
+
+  }
   return (
     <div className="App">
       <h1>Counter app</h1>
       <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
